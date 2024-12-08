bbf95258e3df30b5ce3282b7d7d1cf3f94c8fb95
ANU-COOL <anukulsaini9760@gmail.com>
2024-12-08 17:30:14 +0530

    final changes

---
+++ src/CalenderClass.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+
+public class CalenderClass {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println(c.get(Calendar.HOUR) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND));
+    }
+}
